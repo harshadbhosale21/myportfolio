bde4830ceedf13eb434479324513b9be491f3792
Harshad Bhosale <bhosaleharshad081@gmail.com>
2024-04-16 12:06:55 +0530

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,11 +19,11 @@ const Projects = () => {
                                     <h4>E-Learning Platfrom</h4>
                                     <p>A Centralized online learning platform that offers variety of courses for learners around the world. Build with React.js, Bootstrap for UI and libraraies like Redux and Axios for state management and APIs handling.</p>
                                     <div className="project-item-btns py-2">
-                                        <a className='project-btn mx-2 py-1 px-2 ' href='https://github.com/harshadbhosale21/OnlineLearningPlatform' target='_blank'>
+                                        <a className='project-btn mx-2 py-1 px-2 ' href='https://github.com/harshadbhosale21/OnlineLearningPlatform' target='_blank' rel='noreferrer'>
                                             <span className='px-1'><img src="/images/Repo.png" className='img-fluid' alt="" /></span>
                                             Github
                                         </a>
-                                        <a className='project-btn mx-2 py-1 px-2' href='https://educonnecthb.netlify.app/' target='_blank'>
+                                        <a className='project-btn mx-2 py-1 px-2' href='https://educonnecthb.netlify.app/' target='_blank' rel='noreferrer'>
                                             <span className='px-1'><img src="/images/demo.png" className='img-fluid' alt="" /></span>
                                             Demo
                                         </a>
@@ -38,11 +38,11 @@ const Projects = () => {
                                     <h4>HungryHUb</h4>
                                     <p>Designed and Developed a Restuarant Website that showcases the Hospitality, Delicious menu, and various events that are held at their place. It also allows customers to order Online.</p>
                                     <div className="project-item-btns py-2">
-                                        <a className='project-btn mx-2 py-1 px-2 ' href='https://github.com/harshadbhosale21/OnlineLearningPlatform' target='_blank'>
+                                        <a className='project-btn mx-2 py-1 px-2 ' href='https://github.com/harshadbhosale21/OnlineLearningPlatform' target='_blank' rel='noreferrer'>
                                             <span className='px-1'><img src="/images/Repo.png" className='img-fluid' alt="" /></span>
                                             Github
                                         </a>
-                                        <a className='project-btn mx-2 py-1 px-2' href='https://educonnecthb.netlify.app/' target='_blank'>
+                                        <a className='project-btn mx-2 py-1 px-2' href='https://educonnecthb.netlify.app/' target='_blank' rel='noreferrer'>
                                             <span className='px-1'><img src="/images/demo.png" className='img-fluid' alt="" /></span>
                                             Demo
                                         </a>
@@ -57,11 +57,11 @@ const Projects = () => {
                                     <h4>jetGlobal</h4>
                                     <p>Designed and Developed a Restuarant Website that showcases the Hospitality, Delicious menu, and various events that are held at their place. It also allows customers to order Online.</p>
                                     <div className="project-item-btns py-2">
-                                        <a className='project-btn mx-2 py-1 px-2 ' href='https://github.com/harshadbhosale21/jetSetGlobal' target='_blank'>
+                                        <a className='project-btn mx-2 py-1 px-2 ' href='https://github.com/harshadbhosale21/jetSetGlobal' target='_blank' rel='noreferrer'>
                                             <span className='px-1'><img src="/images/Repo.png" className='img-fluid' alt="" /></span>
                                             Github
                                         </a>
-                                        <a className='project-btn mx-2 py-1 px-2' href='https://jetsetglobalhb.netlify.app/' target='_blank'>
+                                        <a className='project-btn mx-2 py-1 px-2' href='https://jetsetglobalhb.netlify.app/' target='_blank' rel='noreferrer'>
                                             <span className='px-1'><img src="/images/demo.png" className='img-fluid' alt="" /></span>
                                             Demo
                                         </a>
